03d5e0cd1141401b2b492dec409f15a0db15fd59
Samuel Pandohan Terampil Gultom <adalgisoabercio2001@gmail.com>
2024-01-06 16:06:05 +0700

    feat(math): add linalg, mulcal, and pca samples

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements(files: str) -> str:
+    pass
+
+
+setup(
+    name="MLOps Notebooks Playground",
+    version="1.0.0",
+    author="pandohansamuel19"
+)
