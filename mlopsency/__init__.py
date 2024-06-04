51a12dc187779d761040a52c784bc136b3256830
pandohansamuel19 <pandohansamuel19@gmail.com>
2024-06-05 00:52:20 +0700

    update objectiveness

--- mlopsency/__init__.py
+++ mlopsency/__init__.py
@@ -1,31 +1,8 @@
-# Core Scenario
-from mlopsency.core import Core
-from mlopsency.data import Functional
-
-# Runtime Scenario
-from mlopsency.runtime import Dtype, Grad
-from mlopsency.runtime.device import Device
-from mlopsency.runtime.engine import Driver
-from mlopsency.runtime.nn import Layers
-from mlopsency.runtime.ops import Ops
-
-# Operation Scenario
-from mlopsency.operate.accelerate import Accelerate
-from mlopsency.operate.adversarial import Adversarial
-from mlopsency.operate.auto import Automation
-from mlopsency.operate.data import Data
-from mlopsency.operate.orchestra import Orchestration
-
-# Deployment Scenario
-from mlopsency.deploy import Config
-from mlopsency.deploy.aws import AWS
-from mlopsency.deploy.gcp import GoogleCloud
-from mlopsency.deploy.hosting import VirtualMachine, Storage
-
-# Target Scenario
-from mlopsency.target import Compile
-from mlopsency.target.api import API, Backend
-from mlopsency.target.hub import Hub, TFHub, TorchHub, HuggingFaceHub, DockerHub
-from mlopsency.target.lite import LITE, PI, Qualcomm, Flutter
+from mlopsency.cycle import accelerate, adversarial, auto, ops, orchestra, serialization
+from mlopsency.data import data, management, pipeline, store, synthesis
+from mlopsency.report import autoviz, benchmark, explain, statistic
+from mlopsency.runtime import config, device, engine, loss, nn, optim
+from mlopsency.serve import config, docker, hosting, storage
+from mlopsency.target import api, hub, tiny
 
 __version__ = '1.0.0'
