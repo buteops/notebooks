eae6b686f0c3937f6cbb55b83a9390a1adc14cb9
pandohansamuel19 <pandohansamuel19@gmail.com>
2024-06-04 20:32:33 +0700

    refactor core to examples

---
+++ examples/inception.py
@@ -0,0 +1,14 @@
+'''
+  Neural Style Transfer for Inception Model
+'''
+
+from __future__ import annotations
+import tensorflow as tf
+import numpy as np
+from keras import backend as K
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == '__main__':
+  pass
---
+++ examples/saliency.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+from typing import List
+from pathlib import Path
+import sys
+
+
+if __name__ == '__main__':
+  print("Hello")
